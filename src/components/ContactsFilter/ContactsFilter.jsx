31fdcbed4d6ec6ed37d483086aa88f94fff2ce9f
zVladimirz <tzxdlo@gmail.com>
2022-09-25 14:01:28 +0300

    1

--- src/components/ContactsFilter/ContactsFilter.jsx
+++ src/components/ContactsFilter/ContactsFilter.jsx
@@ -1,11 +1,9 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { changeFilter } from 'redux/contacts';
-import { contactsSelectors } from 'redux/contacts';
 import TextField from '@mui/material/TextField';
 
 const ContactsFilter = () => {
   const dispatch = useDispatch();
-  const value = useSelector(contactsSelectors.getFilter);
   const onChange = e => {
     dispatch(changeFilter(e.currentTarget.value));
   };
